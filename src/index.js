82ac392958ce35a98e64ba3fafe6ed8c92e19820
Nefalit <nefalit7@gmail.com>
2022-06-02 15:43:52 +0300

    lesson 02

--- src/index.js
+++ src/index.js
@@ -2,20 +2,58 @@
 // eagles;
 import { findParametr } from './js/api';
 import eventCard from './templates/event-card.hbs';
-const ulEl=document.querySelector(".js-list")
+const ulEl = document.querySelector('.js-list');
+const btnEl = document.querySelector('.js-btn');
+const formInputEl = document.querySelector('.form-input');
+let page = 0;
+let findText = 'nirvana';
+btnEl.addEventListener('click', loadData);
+formInputEl.addEventListener('submit', searchData);
 
-
-findParametr('eagles')
-    .then(data => {
-      const result = eventCard(data._embedded.events);
-      renderList(result);
-            console.log(data);
-
+findParametr(findText, page)
+  .then(data => {
+    const result = eventCard(data._embedded.events);
+    renderList(result);
+    console.log(data);
   })
   .catch(err => {
     console.log(err);
   });
 
 function renderList(string) {
-      ulEl.insertAdjacentHTML("beforeend", string);
+  ulEl.insertAdjacentHTML('beforeend', string);
+}
+function loadData() {
+  page += 1;
+
+  findParametr(findText, page)
+    .then(data => {
+      if (page === data.page.totalPages) {
+        btnEl.classList.add('is-hidden');
+        return;
+      }
+      const result = eventCard(data._embedded.events);
+      renderList(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+function searchData(ev) {
+  ev.preventDefault();
+  page = 0;
+  findText = ev.target.elements.input.value.trim();
+  if (!findText) {
+    console.log('hello');
+    return;
   }
+  ulEl.innerHTML = '';
+  findParametr(findText, page)
+    .then(data => {
+      const result = eventCard(data._embedded.events);
+      renderList(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
--- src/js/api.js
+++ src/js/api.js
@@ -1,16 +1,16 @@
-
 // k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6;
 // eagles;
 
-const API_KEY = "k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6";
+const API_KEY = 'k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6';
 const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
-export const findParametr =(query) => {
-    return fetch(`${ROOT_URL}?apikey=${API_KEY}&keyword=${query}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status)
-        }
-       return response.json()
-    });
-}
-
+export const findParametr = (query, page) => {
+  return fetch(
+    `${ROOT_URL}?apikey=${API_KEY}&keyword=${query}&size=50&page=${page}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
