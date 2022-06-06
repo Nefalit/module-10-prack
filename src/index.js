ba6388e63b06d39739e6d6be892d0afaf59ad642
Nefalit <nefalit7@gmail.com>
2022-06-06 16:13:57 +0300

    prak 03.06

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6;
 // eagles;
-import { findParametr } from './js/api';
+import { findParametr, findCurrentElInfo } from './js/api';
 import eventCard from './templates/event-card.hbs';
+
 const ulEl = document.querySelector('.js-list');
 const btnEl = document.querySelector('.js-btn');
 const formInputEl = document.querySelector('.form-input');
@@ -9,12 +10,12 @@ let page = 0;
 let findText = 'nirvana';
 btnEl.addEventListener('click', loadData);
 formInputEl.addEventListener('submit', searchData);
-
+const backDrop = document.querySelector('.backdrop');
+const modalEl = document.querySelector('.module');
 findParametr(findText, page)
   .then(data => {
     const result = eventCard(data._embedded.events);
     renderList(result);
-    console.log(data);
   })
   .catch(err => {
     console.log(err);
@@ -30,6 +31,7 @@ function loadData() {
     .then(data => {
       if (page === data.page.totalPages) {
         btnEl.classList.add('is-hidden');
+
         return;
       }
       const result = eventCard(data._embedded.events);
@@ -56,4 +58,56 @@ function searchData(ev) {
     .catch(err => {
       console.log(err);
     });
+}
+ulEl.addEventListener('click', onItemCardClick);
+
+function onItemCardClick(event) {
+  if (event.target.nodeName === 'UL') {
+    return;
+  }
+  const currentId = event.target.closest('li').id;
+  findCurrentElInfo(currentId)
+    .then(response => {
+      renderModalContant(response);
+    })
+    .catch(err => console.log(err));
+
+  backDrop.classList.add('is-open');
+  ulEl.removeEventListener('click', onItemCardClick);
+  backDrop.addEventListener('click', onModalCloseClick);
+}
+function onModalCloseClick(event) {
+  if (!event.target.classList.contains('module')) {
+    backDrop.removeEventListener('click', onModalCloseClick);
+    backDrop.classList.remove('is-open');
+    ulEl.addEventListener('click', onItemCardClick);
+  }
+}
+
+function renderModalContant(obj) {
+  const {
+    dates: {
+      start: { localDate, localTime },
+      status: { code },
+      timezone,
+    },
+    images: [{ url: imgUrl }],
+    name,
+    info,
+    ticketLimit: { info: ticketInfo },
+    url: ticketUrl,
+  } = obj;
+  // console.log(info);
+  console.log(ticketUrl);
+
+  const markUp = `<h2 class="title">${name}</h2>
+<img src=${imgUrl} width="400" alt=${name} />
+<p>About: ${info || 'no information'}</p>
+<p>Venue: ${timezone}</p>
+<p>Date and Time: ${localDate}, ${localTime}</p>
+<p>Ticket info: ${ticketInfo}</p>
+<p>Available to buy: ${code}</p>
+<a class="link-buy" rel="noopener noreferrer" href="${ticketUrl}">Buy</a>`;
+
+  modalEl.insertAdjacentHTML('beforeend', markUp);
 }
--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,3 @@
-// k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6;
-// eagles;
-
 const API_KEY = 'k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6';
 const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
@@ -8,6 +5,17 @@ export const findParametr = (query, page) => {
   return fetch(
     `${ROOT_URL}?apikey=${API_KEY}&keyword=${query}&size=50&page=${page}`
   ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
+
+const SecondRoot_URL = 'https://app.ticketmaster.com/discovery/v2/events';
+
+export const findCurrentElInfo = id => {
+  return fetch(`${SecondRoot_URL}/${id}?apikey=${API_KEY}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
